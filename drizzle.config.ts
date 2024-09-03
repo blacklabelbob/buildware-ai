54df797e1a39e8165df6de257ed4fbd80f88f6c6
Bob Wi <118494694+blacklabelbob@users.noreply.github.com>
2024-09-03 15:43:23 -0400

    Final changes before deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL as string, // Ensure this line is present and correctly typed
+    url: process.env.DATABASE_URL as string,
+    ssl: {
+      rejectUnauthorized: false,  // Allows self-signed certificates
+    },
   },
   schema: './db/schema/index.ts',
   out: './db/migrations',
